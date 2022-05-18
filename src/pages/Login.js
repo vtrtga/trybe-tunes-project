2f35748b9caa1ce0e4c07fe092aadae6d9af86d0
vtrtga <vitor.ypvalim@gmail.com>
2022-05-18 15:23:42 -0300

    first

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import { Route } from 'react-router-dom';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        Login
+      </div>
+    );
+  }
+}
